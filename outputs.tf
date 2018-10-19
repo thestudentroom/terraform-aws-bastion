376d1ce4fc22202b5f6caf798ed1d31a1a79793b
Luke Livingstone <luke.livingstone@thestudentroom.com>
2018-10-19 10:27:37 +0100

    Remove output

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "bucket_name" {
   value = "${aws_s3_bucket.bucket.bucket}"
 }
 
-output "elb_ip" {
-  value = "${aws_lb.bastion_lb.dns_name}"
-}
-
 output "private_instances_security_group" {
   value = "${aws_security_group.private_instances_security_group.id}"
 }
